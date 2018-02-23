5806f874a416df9db934e4dbd1d97fbe8a05862d
Fl4v1o <flavio.thebigboss@gmail.com>
2018-02-23 00:39:24 -0300

    DiagonalDifference

--- DevTrainment/Program.cs
+++ DevTrainment/Program.cs
@@ -5,7 +5,20 @@ namespace DevTrainment
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[][] a = new int[n][];
+            for (int a_i = 0; a_i < n; a_i++)
+            {
+                string[] a_temp = Console.ReadLine().Split(' ');
+                a[a_i] = Array.ConvertAll(a_temp, Int32.Parse);
+            }
+            int result = DiagonalDifference.diagonalDifference(a);
+            Console.WriteLine(result);
+        }
+
+        static void Main1(string[] args)
         {
             string[] tokens_a0 = Console.ReadLine().Split(' ');
             int a0 = Convert.ToInt32(tokens_a0[0]);
---
+++ Libs/Easy/DiagonalDifference.cs
@@ -0,0 +1,24 @@
+﻿namespace Libs.Easy
+{
+    public class DiagonalDifference
+    {
+        public static int diagonalDifference(int[][] a)
+        {
+            int diagonalA = 0;
+            int diagonalB = 0;
+            for (int i = 0; i < a[i].Length -1; i++)
+            {
+                for (int j = 0; j < a[j].Length -1; j++)
+                {
+                    if ((i == j))
+                        diagonalA += a[i][j]; 
+
+                    if((i + j) == (a[i].Length -1))
+                        diagonalB += a[i][j];
+                }
+            }
+
+            return diagonalA - diagonalB;
+        }
+    }
+}
