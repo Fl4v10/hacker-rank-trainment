3808561330e00c8afad251a81ad2459538190505
Fl4v1o <flavio.thebigboss@gmail.com>
2018-02-24 10:47:33 -0300

    PlusMinus

--- DevTrainment/Program.cs
+++ DevTrainment/Program.cs
@@ -5,44 +5,69 @@ namespace DevTrainment
 {
     class Program
     {
-        static void Main(String[] args)
+        #region AVeryBigSum
+        //static void Main(String[] args)
+        //{
+        //    int n = Convert.ToInt32(Console.ReadLine());
+        //    string[] ar_temp = Console.ReadLine().Split(' ');
+        //    long[] ar = Array.ConvertAll(ar_temp, Int64.Parse);
+        //    long result = AVeryBigSum.aVeryBigSum(n, ar);
+        //    Console.WriteLine(result);
+
+        //    Console.ReadKey();
+        //}
+        #endregion
+        #region CompareTheTriplets
+        //static void Main(string[] args)
+        //{
+        //    string[] tokens_a0 = Console.ReadLine().Split(' ');
+        //    int a0 = Convert.ToInt32(tokens_a0[0]);
+        //    int a1 = Convert.ToInt32(tokens_a0[1]);
+        //    int a2 = Convert.ToInt32(tokens_a0[2]);
+        //    string[] tokens_b0 = Console.ReadLine().Split(' ');
+        //    int b0 = Convert.ToInt32(tokens_b0[0]);
+        //    int b1 = Convert.ToInt32(tokens_b0[1]);
+        //    int b2 = Convert.ToInt32(tokens_b0[2]);
+        //    int[] result = CompareTheTriplets.Solve(a0, a1, a2, b0, b1, b2);
+        //    Console.WriteLine(String.Join(" ", result));
+
+        //    Console.ReadKey();
+        //}
+        #endregion
+        #region DiagonalDifference
+        //static void Main(String[] args)
+        //{
+        //    int n = Convert.ToInt32(Console.ReadLine());
+        //    int[][] a = new int[n][];
+        //    for (int a_i = 0; a_i < n; a_i++)
+        //    {
+        //        string[] a_temp = Console.ReadLine().Split(' ');
+        //        a[a_i] = Array.ConvertAll(a_temp, Int32.Parse);
+        //    }
+        //    int result = DiagonalDifference.diagonalDifference(a);
+        //    Console.WriteLine(result);
+
+        //    Console.ReadKey();
+        //}
+        #endregion
+        #region PlusMinus
+        //static void Main(String[] args)
+        //{
+        //    int n = Convert.ToInt32(Console.ReadLine());
+        //    string[] arr_temp = Console.ReadLine().Split(' ');
+        //    int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+        //    decimal[] sums = PlusMinus.plusMinus(arr, n);
+        //    Console.WriteLine(sums[0].ToString("0.000000"));
+        //    Console.WriteLine(sums[1].ToString("0.000000"));
+        //    Console.WriteLine(sums[2].ToString("0.000000"));
+
+        //    Console.ReadKey();
+        //}
+        #endregion
+
+        static void Mainn(String[] args)
         {
-            int n = Convert.ToInt32(Console.ReadLine());
-            int[][] a = new int[n][];
-            for (int a_i = 0; a_i < n; a_i++)
-            {
-                string[] a_temp = Console.ReadLine().Split(' ');
-                a[a_i] = Array.ConvertAll(a_temp, Int32.Parse);
-            }
-            int result = DiagonalDifference.diagonalDifference(a);
-            Console.WriteLine(result);
 
-            Console.ReadKey();
-        }
-
-        static void Main1(string[] args)
-        {
-            string[] tokens_a0 = Console.ReadLine().Split(' ');
-            int a0 = Convert.ToInt32(tokens_a0[0]);
-            int a1 = Convert.ToInt32(tokens_a0[1]);
-            int a2 = Convert.ToInt32(tokens_a0[2]);
-            string[] tokens_b0 = Console.ReadLine().Split(' ');
-            int b0 = Convert.ToInt32(tokens_b0[0]);
-            int b1 = Convert.ToInt32(tokens_b0[1]);
-            int b2 = Convert.ToInt32(tokens_b0[2]);
-            int[] result = CompareTheTriplets.Solve(a0, a1, a2, b0, b1, b2);
-            Console.WriteLine(String.Join(" ", result));
-
-            Console.ReadKey();
-        }
-
-        static void Main2(String[] args)
-        {
-            int n = Convert.ToInt32(Console.ReadLine());
-            string[] ar_temp = Console.ReadLine().Split(' ');
-            long[] ar = Array.ConvertAll(ar_temp, Int64.Parse);
-            long result = AVeryBigSum.aVeryBigSum(n, ar);
-            Console.WriteLine(result);
         }
     }
 }
